998b68914e9c884f0a12122514e10511e0a73869
manal <manal.salmi1501@gmail.com>
2021-06-01 12:11:28 +0200

    task n2

--- src/test/java/org/formation/model/FormationTest.java
+++ src/test/java/org/formation/model/FormationTest.java
@@ -7,23 +7,29 @@ import junit.framework.TestCase;
 public class FormationTest extends TestCase {
 	public void testToString()
 	{     
-        assertEquals(
-        	"Formation;20210528;NULL;1;300;Active;Original;NULL",
-        	(new Formation(
-        		"20210528", 
-        		null, 
-        		null,
-        		300,
-        		"Active",
-        		"Original",
-        		1
-        	)).toString()
-        );
-    }
-	
+		assertEquals(
+				"Formation;20210528;NULL;1;300;Active;Original;NULL",
+				(new Formation(
+						"20210528", 
+						null, 
+						null,
+						300,
+						"Active",
+						"Original",
+						1
+						)).toString()
+				);
+	}
+
 	public void testGetAll() {
 		// What we are mainly testing here is that the method call doesn't raise
 		// an error
 		assertTrue(Formation.getAll() instanceof List<?>);
+	}
+
+	public void testIsRemboursee() {
+		assertTrue(new Formation("20210601",null, null, 250, "Remboursee","Original", 666).isRemboursee());
+		assertFalse(new Formation("20210602",null, null, 700, "Annulee","Original", 333).isRemboursee());
+
 	}
 }
