8ed27feba8ef18a120c616e64f268857367eef12
min-tai <mina.taifour@gmail.com>
2021-06-02 14:46:44 +0200

    message commit

--- src/main/java/org/formation/model/Fixture.java
+++ src/main/java/org/formation/model/Fixture.java
@@ -102,7 +102,7 @@ public class Fixture {
 		return newFormateur;
 	}
 	
-	 private static Formation addFormation( Formateur formateur,String statutFormateur) {
+	 private static Formation addFormation( Formateur formateur, String statutFormateur) {
 		 Formateur formateur1 = new Formateur("Formateur1", 2);
 		 Formation formation = new Formation(formateur1, "Annulé"); 
 		 formationFixture.add(formation);
